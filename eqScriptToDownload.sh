261044664e20fad7dd3ecb98cbda678d9c205895
Dimitris Kagialis <kagialisd@gmail.com>
2020-03-11 15:48:54 +0000

    tutorial v.1.0

--- eqScriptToDownload.sh
+++ eqScriptToDownload.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-echo "Test Bash"
-touch total.csv
+echo "Starting the earthquakes data downloading process"
+touch ../data/earthquakes.csv
 for value in {1..12}
 do
   	let 'value1 = value+1'
-	year=2017
+	year=2018
 
-	if [ "$value" = "12"  ]; then                                                                                                                                                                                                                                                                                                                           
-          value1="$value"                                                                                                                                                                  
-        fi 
+	if [ "$value" = "12"  ]; then
+          value1="$value"
+        fi
 
 	if [ "$value" = "1"  ]; then
   	  value="0$value"
 	  value1="0$value1"
 	fi
-        if [ "$value" = "2"  ]; then                                                                                                                                                          
-          value="0$value"  
-          value1="0$value1"                                                                                                                                                                                                                                                                                                                                    
+        if [ "$value" = "2"  ]; then
+          value="0$value"
+          value1="0$value1"
         fi
-	if [ "$value" = "3"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                                   
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "3"  ]; then
+          value1="0$value1"
+          value="0$value"
         fi
-	if [ "$value" = "4"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                         
-	  value="0$value"                                                                                                                                                                     
+	if [ "$value" = "4"  ]; then
+          value1="0$value1"
+	  value="0$value"
         fi
-	if [ "$value" = "5"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                                   
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "5"  ]; then
+          value1="0$value1"
+          value="0$value"
         fi
-	if [ "$value" = "6"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                                   
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "6"  ]; then
+          value1="0$value1"
+          value="0$value"
         fi
-	if [ "$value" = "7"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                                   
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "7"  ]; then
+          value1="0$value1"
+          value="0$value"
         fi
-	if [ "$value" = "8"  ]; then                                                                                                                                                          
-          value1="0$value1"                                                                                                                                                                   
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "8"  ]; then
+          value1="0$value1"
+          value="0$value"
         fi
-	if [ "$value" = "9"  ]; then                                                                                                                                                          
-          value="0$value"                                                                                                                                                                     
+	if [ "$value" = "9"  ]; then
+          value="0$value"
         fi
-	                                                                                                                                                                                                                                                                                                       
+
         value2="$value1"
 
 	for split in {1..2}
@@ -58,46 +58,31 @@ do
            daystart=01
 	   dayend=15
 	   value1="$value"
-	 fi                                                                                                           
-	
+	 fi
+
          if [ "$split" = "2" ]; then
 	   if [ "$value" = "12" ]; then
 		dayend=31
            fi
          fi
-	                  
+
 	echo "year --> $year"
 	echo "month --> $value"
 	echo "dayStart -> $daystart"
 	echo "dayEnd -> $dayend"
 	echo "nextMonth -> $value1"
-     
-#wget -O crawlEarthquake$value$daystart.csv https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=$year-$value-$daystart&endtime=$year-$value1-$dayend&minmagnitude=5
-	echo "https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=$year-$value-$daystart&endtime=$year-$value1-$dayend&minmagnitude=6"  
-	
-	a="earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=$year-$value-$daystart&endtime=$year-$value1-$dayend&minmagnitude=6" 
+
+	echo "https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=$year-$value-$daystart&endtime=$year-$value1-$dayend&minmagnitude=0"
+
+	a="earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=$year-$value-$daystart&endtime=$year-$value1-$dayend&minmagnitude=0"
 	wget -O $value$daystart $a
-	sleep 5s
-	#rm crawlEarthquake$value$daystart.csv
-	
-	cat $value$daystart >> total.csv
-	echo "FINISHED DOWNLOAD PARTIAL the data is saved in total.csv"
+	sleep 2s
+
+	cat $value$daystart >> earthquakes-raw.csv
+	echo "FINISHED DOWNLOAD PARTIAL the data is saved in earthquakes-raw.csv"
 	sleep 1s
 	rm $value$daystart
 	done
 
 done
-echo "FINISHED DOWNLOAD all the data is saved in total.csv"
-
-
-                                                                                                                              
-                                                                                                                                                      
-                                                                                                                                                                                  
-                                                                                                                                                       
-                                                                                                                                                              
-                                                                                                                                                                                           
-                                                                                                                                                            
-                                                                                                                                                                                            
-                                                                                                                                                     
-                                                                                                                                                              
-                                       
+echo "FINISHED DOWNLOAD all the data is saved in earthquakes-raw.csv"
